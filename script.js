76dfde92efd045e285b79f1363fbc109cc3daa71
Eze Ezinne Leticia <ezeezinn42@gmail.com>
2022-11-04 15:53:06 +0100

    sam please check line 150-157 oof script.js for me

--- script.js
+++ script.js
@@ -63,7 +63,8 @@ function catGen() {
 //   message = finalMessage(results);
 //   rgsFrontEnd(yourChoice.id, botChoice, Message); //{"message": "you won!", "color": "green"}
 
-// rock, paper, scissors;
+/* challenge 3  ----   rock, paper, scissors;*/
+
 function rpsGame(yourChoice) {
   // from line 67 to line 75 is more like the skeleton and the procedures
   console.log(yourChoice); //to get the answer to the body, which is the body we are supposed to fill up
@@ -73,6 +74,10 @@ function rpsGame(yourChoice) {
   console.log(botChoice);
   results = decideWinner(humanChoice, botChoice);
   console.log(results);
+  message = finalMessage(results);
+  console.log(message);
+
+  rpsfrontend(yourChoice.id, botChoice, message);
 }
 
 function randToRpsInt() {
@@ -85,9 +90,9 @@ function numberToChoice(number) {
 
 function decideWinner(yourChoice, botChoice) {
   var rpsDatabase = {
-    "rock": { "scissors": 1, "rock": 0.5, "paper": 0 },
-    "paper": { "rock": 1, "paper": 0.5, "scissors": 0 },
-    "scissors": { "paper": 1," scissors": 0.5, "rock": 0 },
+    rock: { scissors: 1, rock: 0.5, paper: 0 },
+    paper: { rock: 1, paper: 0.5, scissors: 0 },
+    scissors: { paper: 1, " scissors": 0.5, rock: 0 },
   };
   var yourScore = rpsDatabase[yourChoice][botChoice];
   var botScore = rpsDatabase[botChoice][yourChoice];
@@ -97,12 +102,83 @@ function decideWinner(yourChoice, botChoice) {
 
 function finalMessage([yourScore, botScore]) {
   if (yourScore === 0) {
-    return {"message": "you lost!", "color": "red"};
+    return { message: "you lost!", color: "red" };
   } else if (yourScore === 0.5) {
-    return {"message": "you tied!", "color": "yellow"};
+    return { message: "you tied!", color: "yellow" };
   } else {
-    return {"message": "you won!", "color": "green"};
+    return { message: "you won!", color: "green" };
   }
 }
 
+function rpsfrontend(humanImageChoice, botImageChoice, finalMessage) {
+  var imagesDatabase = {
+    rock: document.getElementById("rock").src,
+    paper: document.getElementById("paper").src,
+    scissors: document.getElementById("scissors").src,
+  };
 
+  // lets remove all the images
+  document.getElementById("rock").remove();
+  document.getElementById("paper").remove();
+  document.getElementById("scissors").remove();
+
+  var humanDiv = document.createElement("div");
+  var botDiv = document.createElement("div");
+  var messageDiv = document.createElement("div");
+
+  humanDiv.innerHTML =
+    "<img src='" +
+    imagesDatabase[humanImageChoice] +
+    "'height=150 width=150 style='box-shadow:0px 10px 50px #000'>";
+
+  messageDiv.innerHTML =
+    "<h1 style='color: " +
+    finalMessage["color"] +
+    "; font-size:60px; box-shadow:0px 10px 50px #00e; padding:30px;'>" +
+    finalMessage["message"] +
+    "</h1>";
+
+  botDiv.innerHTML =
+    "<img src='" +
+    imagesDatabase[botImageChoice] +
+    "'height=150 width=150 style='box-shadow:0px 10px 50px #f0ff'>";
+  document.getElementById("rpsimages").appendChild(botDiv);
+  document.getElementById("rpsimages").appendChild(messageDiv);
+  document.getElementById("rpsimages").appendChild(humanDiv);
+}
+
+// challenge four: change the color of all buttons!!!
+var all_buttons = document.getElementsByTagName("button");
+console.log(all_buttons);
+var copyAllButtons = [];
+for (let i = 0; i < all_buttons.length; i++) {
+  copyAllButtons.push(all_buttons[i].classList[1]);
+  console.log(copyAllButtons);
+}
+console.log(copyAllButtons);
+// all_buttons.classList;
+// console.log(all_buttons.classList);
+function buttonColorChange(buttonThingy) {
+  if (buttonThingy.value === "red") {
+    buttonsRed();
+  } else if (buttonThingy.value === "green") {
+    buttonGreen();
+  } else if (buttonThingy.value === "reset") {
+    buttonColorReset();
+  } else if (buttonThingy.value === "random") {
+    randomColors();
+  }
+}
+
+function buttonsRed() {
+  for (let i = 0; i < all_buttons.length; i++) {
+    all_buttons[i].classList.remove(all_buttons[i].classList[i]);
+    all_buttons[i].classList.add("red");
+  }
+}
+function buttonGreen() {
+  for (let i = 0; i < all_buttons.length; i++) {
+    all_buttons[i].classList.remove(all_buttons[i].classList[i]);
+    all_buttons[i].classList.add("green");
+  }
+}
